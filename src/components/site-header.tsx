2c4d37702b9cb6a2d63fcf92a3a6a913d16839ed
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-12-17 03:40:59 +0100

    Add link to home page in site header

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,13 +1,17 @@
+import Link from 'next/link'
+
 import { Gamepad2 } from 'lucide-react'
 
 export default function SiteHeader() {
   return (
     <header className='py-4 border-b'>
       <div className='container flex items-center'>
-        <h1 className='scroll-m-20 text-2xl font-extrabold tracking-tight flex items-center'>
-          <Gamepad2 className='mr-2' size={30} />
-          Minigames
-        </h1>
+        <Link href='/'>
+          <h1 className='scroll-m-20 text-2xl font-extrabold tracking-tight flex items-center'>
+            <Gamepad2 className='mr-2' size={30} />
+            Minigames
+          </h1>
+        </Link>
       </div>
     </header>
   )
